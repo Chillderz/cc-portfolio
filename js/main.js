f93d0eecf50130f9d1072e54deac90c6a7af3fa2
Chris Childers <cchilders1289@gmail.com>
2020-03-28 11:37:53 -0400

    Added a new component for the modal, although it's not interactive yet.  Some tests came with it too

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ import ExampleWork from './example-work';
 const myWork = [
     {
         'title': "Work Example",
+        'href': "https://example.com",
+        'desc': "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
         'image': {
             'desc': "example screenshot of a project involving code",
             'src': "images/example1.png",
@@ -14,6 +16,8 @@ const myWork = [
     },
     {
         'title': "Portfolio Boilerplate",
+        'href': "https://example.com",
+        'desc': "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
         'image': {
             'desc': "A Serverless Portfolio",
             'src': "images/Portfolio.png",
@@ -22,6 +26,8 @@ const myWork = [
     },
     {
         'title': "Work Example",
+        'href': "https://example.com",
+        'desc': "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
         'image': {
             'desc': "example screenshot of a project involving cats",
             'src': "images/example3.png",
